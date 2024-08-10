3b047b4d490d7f8c8bfb4f88afa8809d70453431
ysl <116336464+li-lyx@users.noreply.github.com>
2024-08-10 22:07:11 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-    <router-view></router-view>
+    <div id="app">
+    <Sidebar />
+    <Login />
+  </div>
 </template>
 
-<script setup lang="ts">
-
+<script setup>
+import MainLayout from './components/MainLayout.vue'
+import Sidebar from './components/Sidebar.vue';
+import Login from './components/Login.vue';
 </script>
+
+<style>
+/* 全局样式 */
+
+/* 媒体查询: 对于较小的屏幕 */
+@media (max-width: 768px) {
+  /* 如果需要特定的全局样式调整，可以在这里添加 */
+}
+</style>
