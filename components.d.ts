92c3447a8d799d9ccc33e0d357fabfc86eb0bd67
ysl <m2843876602@outlook.com>
2024-08-12 23:10:47 +0800

    添加下拉框

--- components.d.ts
+++ components.d.ts
@@ -25,6 +25,7 @@ declare module 'vue' {
     Message: typeof import('primevue/message')['default']
     Panel: typeof import('primevue/panel')['default']
     PanelMenu: typeof import('primevue/panelmenu')['default']
+    Password: typeof import('primevue/password')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Select: typeof import('primevue/select')['default']
