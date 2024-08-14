05f189f53405334a92890e2b33a754f9ac4081c7
Chengcheng Hu <Chengchenghu0728@gmail.com>
2024-08-15 07:59:37 +0800

    ✨ 更新生日问题和登录界面

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
-    <div id="app">
-    <Sidebar />
-    <Login />
-  </div>
+    <router-view></router-view>
 </template>
 
-<script setup>
-import MainLayout from './components/MainLayout.vue'
-import Sidebar from './components/Sidebar.vue';
-import Login from './components/Login.vue';
+<script setup lang="ts">
+
 </script>
-
-<style>
-/* 全局样式 */
-
-/* 媒体查询: 对于较小的屏幕 */
-@media (max-width: 768px) {
-  /* 如果需要特定的全局样式调整，可以在这里添加 */
-}
-</style>
