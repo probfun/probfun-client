a65df91b4ac58fd26ca269a1c26c96bc4de8ea3c
ysl <m2843876602@outlook.com>
2024-08-14 11:03:45 +0800

    quick changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import PrimeVue from 'primevue/config';
 import router from '@/router'
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://8.154.34.171';
 
 const app = createApp(App);
 
