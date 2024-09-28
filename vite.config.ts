b7d324a92dc0b10c83058a75b084491df25d3e8e
Chengcheng Hu <Chengchenghu0728@gmail.com>
2024-09-28 23:03:17 +0800

    🚀 啊？？？？？

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,36 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  return {
-    optimizeDeps: {
-      exclude: ['js-big-decimal', ''],
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ['js-big-decimal', ''],
+  },
+  base: './',
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        PrimeVueResolver(),
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    plugins: [
-      vue(),
-      Components({
-        resolvers: [
-          PrimeVueResolver(),
-        ],
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': '/src',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://8.154.34.171:8080',
+        // target: 'http://127.0.0.1:4523/m1/4811506-4465979-default',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://8.154.34.171:8080',
+        changeOrigin: true,
+        secure: false,
       },
     },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://8.154.34.171:8080',
-          // target: 'http://127.0.0.1:4523/m1/4811506-4465979-default',
-          changeOrigin: true,
-        },
-        '/uploads': {
-          target: 'http://8.154.34.171:8080',
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  }
+  },
 })
