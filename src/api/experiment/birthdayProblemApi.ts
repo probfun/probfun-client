90970ae4314812f17c6fd2ce0cc1b8ab4827185e
ysl <m2843876602@outlook.com>
2024-10-06 19:47:45 +0800

    🐛 修复Build错误

--- src/api/experiment/birthdayProblemApi.ts
+++ src/api/experiment/birthdayProblemApi.ts
@@ -2,7 +2,7 @@ import { post } from "../request";
 import type { BirthdayProblem } from "./birthdayProblemType";
 
 export async function birthdayProblemApi(people: number, runs: number, monthProbs: number[], needNProb: boolean) {
-    const result = await post<{ birthdayProblem: BirthdayProblem }>('/api/experiment/birthdayProblem', {
+    const result = await post<BirthdayProblem>('/api/experiment/birthdayProblem', {
         people,
         runs,
         monthProbs,
