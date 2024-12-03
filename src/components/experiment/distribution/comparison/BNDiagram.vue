7f3f7725b423b1e87ed57adb50b223d67adcc690
ysl <m2843876602@outlook.com>
2024-12-03 10:37:13 +0800

    离散的图改成竖线

--- src/components/experiment/distribution/comparison/BNDiagram.vue
+++ src/components/experiment/distribution/comparison/BNDiagram.vue
@@ -10,6 +10,8 @@ const props = defineProps<{
     p: number; // 成功概率
 }>();
 
+let idNumber = 0;
+
 onMounted(() => {
     const options = {
         keypad: false,
@@ -27,19 +29,43 @@ onMounted(() => {
 const drawDistributions = () => {
     if (!calculator) return;
 
+    const binomialValue: any[] = [];
+
+    const binomialPoints = Array.from({ length: props.n + 1 }, (_, k) => {
+        const binomialCoefficient = factorial(props.n) / (factorial(k) * factorial(props.n - k));
+        const probability = (binomialCoefficient * (props.p ** k) * ((1 - props.p) ** (props.n - k))).toFixed(4);
+        binomialValue.push(probability);
+        return `(${k}, ${probability})`;
+    });
+
+    function factorial(n: any): any {
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+
     const normal = {
         id: 'normal',
         latex: `f(x) = \\frac{1}{\\sqrt{2\\pi} \\cdot ${Math.sqrt(props.n * props.p * (1 - props.p))}} e^{-\\frac{(x - ${props.n * props.p})^2}{2 \\cdot ${props.n * props.p * (1 - props.p)}}} \\{x >= 0\\}`,
         color: Desmos.Colors.BLUE
     };
 
-    const binomial = {
-        id: 'binomial',
-        latex: `f(x) = \\frac{${props.n}!}{x!((${props.n} - x)!)} ${props.p}^{x} (1 - ${props.p})^{${props.n} - x} \\{x >= 0\\}`,
-        color: Desmos.Colors.RED
+    const binomialPointExpression = {
+        id: `points_${idNumber}`,
+        latex: `(${binomialPoints.join('), (')})`,
+        color: Desmos.Colors.RED,
+        pointSize: 8 // 设置点的大小
+    };
+    for (let i = 0; i <= props.n; i++) {
+        const binomialLine = {
+            id: `lines_${i}`,
+            latex: `x = ${i} \\{0<= y <= ${binomialValue[i]}\\}`,
+            color: Desmos.Colors.RED,
+            lineStyle: Desmos.Styles.DASHED
+        }
+        calculator.setExpression(binomialLine)
     }
+
     calculator.setExpression(normal);
-    calculator.setExpression(binomial);
+    calculator.setExpression(binomialPointExpression);
 
     // 设置图形边界
     calculator.setMathBounds({
@@ -60,5 +86,4 @@ watch(() => [props.n, props.p], () => {
     <div id="elt" ref="elt" class="w-full h-full" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
